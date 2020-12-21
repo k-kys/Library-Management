ef6cc7e7c1ef6bc7653742d476f8afa3f104c6be
k-kys <khuong.kiyoshi@gmail.com>
2020-12-21 15:43:13 +0700

    fix search

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -77,7 +77,7 @@ class BookController extends Controller
         $keyword = $request->get('keyword');
         $query = Book::query();
         if ($keyword) {
-            $query->where('name', 'like', "\'%".$keyword."%\'")->orWhere('id', $keyword);
+            $query->where('name', 'like', "'%".$keyword."%'")->orWhere('id', $keyword);
         }
         $books = $query->orderBy('id', 'desc')->paginate(3);
         return view('admin.manage-book', compact('books'));
